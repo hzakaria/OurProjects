281c62b24ebd0a326a758f0df4830f879430a69a
Zakaria haqiq <zakaria.haqiq01@gmail.com>
2014-03-19 20:37:52 +0000

    new projet

---
+++ e-examen-master/e-examen-configurations/src/main/java/org/e/examen/configurations/App.java
@@ -0,0 +1,13 @@
+package org.e.examen.configurations;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
