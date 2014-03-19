e29302bb367a854e4665da25ca520db2a2870b45
Zakaria haqiq <zakaria.haqiq01@gmail.com>
2014-03-19 20:32:42 +0000

    new projet

---
+++ e-examen-master/e-examen-modele/src/main/java/org/e/examen/modele/ModelTest.java
@@ -0,0 +1,13 @@
+package org.e.examen.modele;
+
+/**
+ * Hello world!
+ *
+ */
+public class ModelTest 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
